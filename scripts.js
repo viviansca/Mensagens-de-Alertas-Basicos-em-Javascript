6bf7bd999ab2dc33df352a24748c6e3b850386ad
Vívian Araújo <119790198+viviansca@users.noreply.github.com>
2023-02-01 08:04:01 -0300

    Commit Full

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('Hello World JavaScript!')
+
+//console
+console.error('Este é um erro!')
+console.warn('Este é um aviso!')
+console.log('Esta é uma mensagem!')
+
+//variaáveis
+var x = 10
+//maneiras mais modernas
+let y = 15
+const z = 20
+
+if (1) {
+    let y = 55
+}
